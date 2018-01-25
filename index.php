436d8477570eea7a15a08ad49f6c81a412bafa8c
root <root@master.g-able.dr.cloud>
2018-01-25 14:19:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+ <title>PHP Application</title>
+ </head>
+ <body bgcolor=Orange>
+ <h1>Congratulations! This is production Version Three</h1>
+        <p>Your <em>PHP</em> application is now running on kubernetes</p>
+        <p>You are running PHP version <?= phpversion() ?></p>
+        <p>You are running PHP on HOST: <?= gethostname() ?> </p>
+ </body>
+</html>
+
