99b8f9d2a07d500ada59b0b860ef5dff08f0b921
root <root@master.g-able.dr.cloud>
2018-01-28 13:56:10 +0700

    update1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  <title>PHP Application</title>
  </head>
- <body bgcolor=Orange>
+ <body bgcolor=Yellow>
  <h1>Congratulations! This is production Version: CHANGEIT </h1>
         <p>Your <em>PHP</em> application is now running on kubernetes</p>
         <p>You are running PHP version <?= phpversion() ?></p>
